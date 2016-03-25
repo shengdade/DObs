56e1993b478a9e5631d7aa8bdb17656e91be2d77
Dade <dade@live.cn>
2016-03-25 14:41:57 -0400

    add EventRecord to database

--- app/src/main/java/com/example/dobs/Activities/EventActivity.java
+++ app/src/main/java/com/example/dobs/Activities/EventActivity.java
@@ -1,23 +1,35 @@
 package com.example.dobs.Activities;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.example.dobs.Classes.EventRecord;
 import com.example.dobs.R;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class EventActivity extends AppCompatActivity {
     private static final String TAG = " EventActivity";
-    public static EventRecord eventRecord;
+    private Context context;
+    CheckBox checkFall;
+    CheckBox checkPRN;
+    CheckBox checkAggressive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        //eventRecord = new EventRecord();
+        this.context = this;
+        checkFall = (CheckBox) findViewById(R.id.checkFall);
+        checkPRN = (CheckBox) findViewById(R.id.checkPRN);
+        checkAggressive = (CheckBox) findViewById(R.id.checkAggressive);
     }
 
     @Override
@@ -30,9 +42,9 @@ public class EventActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_okay:
-//                Toast.makeText(this, "This button would store events later", Toast.LENGTH_SHORT).show();
+                EventRecord record = new EventRecord(getTime(), checkFall.isChecked(), checkPRN.isChecked(), checkAggressive.isChecked());
+                new AddEventTask().execute(record);
                 finish();
-                Toast.makeText(this, "Record stored", Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
@@ -45,5 +57,29 @@ public class EventActivity extends AppCompatActivity {
         MenuItem item = menu.findItem(R.id.menu_add);
         item.setVisible(false);
         return super.onPrepareOptionsMenu(menu);
+    }
+
+    private Calendar getTime() {
+        int[] hourMinutes = getIntent().getIntArrayExtra(CollectActivity.tagEvent);
+        Calendar time = GregorianCalendar.getInstance();
+        time.set(Calendar.HOUR_OF_DAY, hourMinutes[0]);
+        time.set(Calendar.MINUTE, hourMinutes[1]);
+        time.set(Calendar.SECOND, 0);
+        time.set(Calendar.MILLISECOND, 0);
+        return time;
+    }
+
+    private class AddEventTask extends AsyncTask<EventRecord, Void, Void> {
+
+        protected Void doInBackground(EventRecord... records) {
+            if (!isCancelled()) {
+                MainActivity.db.addEventRecord(records[0]);
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void result) {
+            Toast.makeText(context, "Incident stored", Toast.LENGTH_SHORT).show();
+        }
     }
 }
